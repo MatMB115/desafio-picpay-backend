3d9fad819053bf21bf8beef4fde4920716b92f80
MatMB115 <MatMB115@users.noreply.github.com>
2023-08-17 00:18:36 -0300

    feat: create controllers and treating exceptions

---
+++ src/main/java/com/simplifiedpicpay/domain/infra/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.simplifiedpicpay.domain.infra;
+
+import com.simplifiedpicpay.domain.dtos.ExceptionDTO;
+import com.simplifiedpicpay.domain.dtos.TransactionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDTO> threatDuplciateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
